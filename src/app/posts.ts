f0f371719239e800d426921ae3bd3c853167fe82
midzer <midzer@gmail.com>
2017-06-03 15:34:25 +0200

    fix post dates

--- src/app/posts.ts
+++ src/app/posts.ts
@@ -8,97 +8,97 @@ export var POSTS: Post[] = [
   },
   {
       name: 'Lockpickingsimulator 2017',
-      date: '2012/04/23',
+      date: '2017/05/15',
       category: 'Philosophisches'
   },
   {
       name: 'Pausenfiller',
-      date: '2012/04/23',
+      date: '2017/01/17',
       category: 'DJ Sets'
   },
   {
       name: 'Bergfestzelebration III',
-      date: '2012/04/23',
+      date: '2016/11/30',
+      category: 'DJ Sets'
+  },
+  {
+      name: 'Marionettenzauber',
+      date: '2016/08/06',
       category: 'DJ Sets'
   },
   {
       name: 'Darknet von Daniel Suarez',
-      date: '2012/04/23',
+      date: '2016/07/20',
       category: 'Zitate'
   },
   {
-      name: 'Marionettenzauber',
-      date: '2012/04/23',
-      category: 'DJ Sets'
-  },
-  {
       name: 'Midnight Flow',
-      date: '2012/04/23',
+      date: '2016/05/14',
       category: 'DJ Sets'
   },
   {
       name: 'Crème bavaroise',
-      date: '2012/04/23',
+      date: '2016/04/23',
       category: 'DJ Sets'
   },
   {
       name: 'Go pr0 - Mind wanking',
-      date: '2012/04/23',
+      date: '2016/04/09',
       category: 'Philosophisches'
   },
   {
       name: 'Sandgefluester',
-      date: '2012/04/23',
+      date: '2016/03/24',
       category: 'DJ Sets'
   },
   {
       name: 'Guacamole',
-      date: '2012/04/23',
+      date: '2016/03/10',
       category: 'Rezepte'
   },
   {
       name: 'Ojja mit Tbikha de Sfax',
-      date: '2012/04/23',
+      date: '2016/02/08',
       category: 'Rezepte'
   },
   {
       name: 'Bergfestzelebration II',
-      date: '2012/04/23',
+      date: '2016/01/27',
       category: 'DJ Sets'
   },
   {
       name: 'Quinoa Pfanne',
-      date: '2012/04/23',
+      date: '2016/01/16',
       category: 'Rezepte'
   },
   {
       name: 'Groovesmoothie',
-      date: '2012/04/23',
+      date: '2015/12/03',
       category: 'DJ Sets'
   },
   {
       name: 'Kopfgeister',
-      date: '2012/04/23',
+      date: '2015/11/04',
       category: 'DJ Sets'
   },
   {
       name: 'Rauchen als Akt des passiven Widerstands',
-      date: '2012/04/23',
+      date: '2015/10/21',
       category: 'Philosophisches'
   },
   {
       name: 'Inner Vision',
-      date: '2012/04/23',
+      date: '2015/10/13',
       category: 'DJ Sets'
   },
   {
       name: 'Endoplasmatisches Retikolum',
-      date: '2012/04/23',
+      date: '2015/09/02',
       category: 'DJ Sets'
   },
   {
       name: 'Bergfestzelebration',
-      date: '2012/04/23',
+      date: '2015/08/21',
       category: 'DJ Sets'
   }
 ];
