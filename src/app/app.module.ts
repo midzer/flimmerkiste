6a5720e9d8722f74cc30f26bc04bed01841268b3
midzer <midzer@gmail.com>
2018-04-19 15:12:00 +0200

    simplyfy routing logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { MarkdownModule } from 'angular2-markdown';
-import { RoutingModule } from './routing/routing.module';
 
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
@@ -16,6 +16,11 @@ import { Blank2dashPipe } from './blank2dash.pipe';
 import { APP_CONFIG, APP_DI_CONFIG } from "./app-config";
 import { ContentComponent } from './content/content.component';
 import { Mp3filePipe } from './mp3file.pipe';
+
+const routes: Routes = [
+  { path: '', component: OverviewComponent },
+  { path: ':name', component: ContentComponent }
+];
 
 @NgModule({
   declarations: [
@@ -33,8 +38,8 @@ import { Mp3filePipe } from './mp3file.pipe';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MarkdownModule.forRoot(),
-    RoutingModule
+    RouterModule.forRoot(routes),
+    MarkdownModule.forRoot()
   ],
   providers: [
     {
