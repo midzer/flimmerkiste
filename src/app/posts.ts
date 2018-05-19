9d830fd5b227a0aa4cf9380903543ffbd984a82d
midzer <midzer@gmail.com>
2018-05-19 15:01:06 +0200

    new post

--- src/app/posts.ts
+++ src/app/posts.ts
@@ -1,139 +1,144 @@
 import { Post } from './post';
 
 export var POSTS: Post[] = [
-  {
-      name: 'HowTo optimize websites for performance',
-      date: '2018/04/19',
-      category: 'Tutorials'
-  },  
-  {
-      name: 'Prielcrafted',
-      date: '2018/04/08',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Roaming Frequencies',
-      date: '2018/02/05',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Das Wort zum Sonntag',
-      date: '2018/01/28',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Transzendentales Potpourri',
-      date: '2017/12/15',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Relative Gedankennavigation',
-      date: '2017/07/18',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Oachkatzlbeats',
-      date: '2017/07/02',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Die Schlosseiche von Eisolzried',
-      date: '2017/06/03',
-      category: 'Audio'
-  },
-  {
-      name: 'Lockpickingsimulator 2017',
-      date: '2017/05/15',
-      category: 'Philosophisches'
-  },
-  {
-      name: 'Pausenfiller',
-      date: '2017/01/17',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Bergfestzelebration III',
-      date: '2016/11/30',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Marionettenzauber',
-      date: '2016/08/06',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Darknet von Daniel Suarez',
-      date: '2016/07/20',
-      category: 'Zitate'
-  },
-  {
-      name: 'Midnight Flow',
-      date: '2016/05/14',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Crème bavaroise',
-      date: '2016/04/23',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Go pr0 - Mind wanking',
-      date: '2016/04/09',
-      category: 'Philosophisches'
-  },
-  {
-      name: 'Sandgefluester',
-      date: '2016/03/24',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Guacamole',
-      date: '2016/03/10',
-      category: 'Rezepte'
-  },
-  {
-      name: 'Ojja mit Tbikha de Sfax',
-      date: '2016/02/08',
-      category: 'Rezepte'
-  },
-  {
-      name: 'Bergfestzelebration II',
-      date: '2016/01/27',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Quinoa Pfanne',
-      date: '2016/01/16',
-      category: 'Rezepte'
-  },
-  {
-      name: 'Groovesmoothie',
-      date: '2015/12/03',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Kopfgeister',
-      date: '2015/11/04',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Rauchen als Akt des passiven Widerstands',
-      date: '2015/10/21',
-      category: 'Philosophisches'
-  },
-  {
-      name: 'Inner Vision',
-      date: '2015/10/13',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Endoplasmatisches Retikolum',
-      date: '2015/09/02',
-      category: 'DJ Sets'
-  },
-  {
-      name: 'Bergfestzelebration',
-      date: '2015/08/21',
-      category: 'DJ Sets'
-  }
+    {
+        name: 'Retro Dreams',
+        date: '2018/05/19',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'HowTo optimize websites for performance',
+        date: '2018/04/19',
+        category: 'Tutorials'
+    },
+    {
+        name: 'Prielcrafted',
+        date: '2018/04/08',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Roaming Frequencies',
+        date: '2018/02/05',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Das Wort zum Sonntag',
+        date: '2018/01/28',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Transzendentales Potpourri',
+        date: '2017/12/15',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Relative Gedankennavigation',
+        date: '2017/07/18',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Oachkatzlbeats',
+        date: '2017/07/02',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Die Schlosseiche von Eisolzried',
+        date: '2017/06/03',
+        category: 'Audio'
+    },
+    {
+        name: 'Lockpickingsimulator 2017',
+        date: '2017/05/15',
+        category: 'Philosophisches'
+    },
+    {
+        name: 'Pausenfiller',
+        date: '2017/01/17',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Bergfestzelebration III',
+        date: '2016/11/30',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Marionettenzauber',
+        date: '2016/08/06',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Darknet von Daniel Suarez',
+        date: '2016/07/20',
+        category: 'Zitate'
+    },
+    {
+        name: 'Midnight Flow',
+        date: '2016/05/14',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Crème bavaroise',
+        date: '2016/04/23',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Go pr0 - Mind wanking',
+        date: '2016/04/09',
+        category: 'Philosophisches'
+    },
+    {
+        name: 'Sandgefluester',
+        date: '2016/03/24',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Guacamole',
+        date: '2016/03/10',
+        category: 'Rezepte'
+    },
+    {
+        name: 'Ojja mit Tbikha de Sfax',
+        date: '2016/02/08',
+        category: 'Rezepte'
+    },
+    {
+        name: 'Bergfestzelebration II',
+        date: '2016/01/27',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Quinoa Pfanne',
+        date: '2016/01/16',
+        category: 'Rezepte'
+    },
+    {
+        name: 'Groovesmoothie',
+        date: '2015/12/03',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Kopfgeister',
+        date: '2015/11/04',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Rauchen als Akt des passiven Widerstands',
+        date: '2015/10/21',
+        category: 'Philosophisches'
+    },
+    {
+        name: 'Inner Vision',
+        date: '2015/10/13',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Endoplasmatisches Retikolum',
+        date: '2015/09/02',
+        category: 'DJ Sets'
+    },
+    {
+        name: 'Bergfestzelebration',
+        date: '2015/08/21',
+        category: 'DJ Sets'
+    }
 ];
---
+++ src/assets/markdown/retro-dreams.md
@@ -0,0 +1,26 @@
+# Retro Dreams
+
+* Datum: 2018.05.19
+* DJ: tinkelow
+* Genre: Synth, Electronic, 8bit, Trance
+* Laufzeit: 1:09:38
+
+01. 0:00:00 Mire. - Bury
+02. 0:03:56 VHS Dreams - Nightdrive (Club Mix)
+03. 0:08:56 Mega Drive - Video Stalker
+04. 0:12:44 M|O|O|N - Crystals
+05. 0:16:44 Räuberhöhle feeding Tobiah - Lullaby
+06. 0:19:24 Chris Hülsbeck - Katakis (Song 3)
+07. 0:20:38 Demoscene Time Machine - Astronaut
+08. 0:25:18 Sputnik Booster - Firestorm
+09. 0:28:36 Matt Gray - The Street Loading Theme
+10. 0:35:00 Maru - Electro Track 02
+11. 0:39:16 Ferry Corsten - Punk (Radio Edit)
+12. 0:42:28 Tom Pulse feat. Ron Van Den Beuken - Rise up Tonite (Tom & Kim Remix)
+13. 0:45:24 Scotty - The Black Pearl (Dave Darell Radio Edit)
+14. 0:48:54 Yamada feat. Elle Vee - Ordinary (Extended Mix)
+15. 0:52:00 Robyn - Cobrastyle
+16. 0:56:10 Interactive - Forever Young (Kosmonova Radio Edit)
+17. 0:59:34 Members Of Mayday - Culture Flash (Short)
+18. 1:02:34 Kai Tracid - Message Without Words (Album Version)
+19. 1:06:08 Rocco - Generation In Love (Single Version)
