f5a4905935aebd8bf05f558c86e622d7f5180bf5
midzer <midzer@gmail.com>
2019-12-29 15:56:08 +0100

    upgrade to Angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
 
 import { NgxMdModule } from 'ngx-md';
@@ -37,7 +37,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     RouterModule.forRoot(routes),
     NgxMdModule.forRoot()
   ],
