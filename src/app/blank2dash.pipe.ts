acf392749cc492d809377774c4cc0dfcfde0053c
midzer <midzer@gmail.com>
2022-05-27 15:56:07 +0200

    remove commas

--- src/app/blank2dash.pipe.ts
+++ src/app/blank2dash.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class Blank2dashPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return value.split(' ').join('-');
+    return value.split(' ').join('-').replace(',', '');
   }
 
 }
