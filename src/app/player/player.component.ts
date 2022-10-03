891c8ad474e40dc24b4b87583c342820c59e1b5d
midzer <midzer@gmail.com>
2022-10-03 22:59:53 +0200

    delay load sids

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -29,115 +29,18 @@ export class PlayerComponent implements OnInit {
   sids : string[];
   selectedSid : string = 'Last_Ninja_2';
 
-  constructor() {
-    this.sids = [
-      "ACE_II",
-      "Antics_Dulcedo_Cogitationis",
-      "Arkanoid",
-      "Ark_Pandora",
-      "Armalyte",
-      "Auf_Wiedersehen_Monty",
-      "Baby_of_Can_Guru",
-      "Batman_long",
-      "Battle_Valley",
-      "Bionic_Commando",
-      "BMX_Kidz",
-      "Bulldog",
-      "Chordian",
-      "Cobra",
-      "Combat_Crazy",
-      "Comic_Bakery",
-      "Commando",
-      "Compleeto",
-      "Cooperation_Demo",
-      "Crazy_Comets_remix",
-      "Crazy_Comets",
-      "Cybernoid_II",
-      "Cybernoid",
-      "Defender_of_the_Crown",
-      "Delta",
-      "DNA_Warrior",
-      "Driller",
-      "Eliminator",
-      "FAME_1",
-      "Gauntlet_III",
-      "Gerry_the_Germ",
-      "Ghosts_n_Goblins",
-      "Ghouls_n_Ghosts",
-      "Glider_Rider",
-      "Grand_Prix_Circuit",
-      "Great_Giana_Sisters",
-      "Green_Beret",
-      "Hawkeye",
-      "Hysteria",
-      "IK_plus",
-      "International_Karate",
-      "Katakis",
-      "Kentilla",
-      "Kinetix",
-      "Knucklebusters",
-      "Last_Ninja",
-      "Last_Ninja_2",
-      "Last_Ninja_3",
-      "Lightforce",
-      "Master_of_Magic",
-      "Mega_Apocalypse",
-      "Miami_Vice",
-      "Mikie",
-      "Monty_on_the_Run",
-      "Mutants",
-      "Myth",
-      "Nemesis_the_Warlock",
-      "Nightdawn",
-      "Ocean_Loader_3",
-      "One_Man_and_his_Droid",
-      "Parallax",
-      "Platoon",
-      "Rambo_First_Blood_Part_II",
-      "Ramparts",
-      "R_I_S_K",
-      "RoboCop",
-      "RoboCop_3",
-      "Rocky_Star",
-      "R-Type",
-      "Sanxion",
-      "Savage",
-      "Scroll_Machine",
-      "Scumball",
-      "Short_Circuit",
-      "Snare",
-      "Spellbound",
-      "Stormlord",
-      "Supremacy",
-      "Sweet",
-      "Synth_Sample",
-      "SYS4096",
-      "Terra_Cresta",
-      "Tetris",
-      "Thing_Bounces_Back",
-      "Thrust",
-      "Thunderforce",
-      "Times_of_Lore",
-      "To_be_on_Top",
-      "Trap",
-      "Turbo_Outrun",
-      "Uridium",
-      "Vikings",
-      "Warhawk",
-      "Way_of_the_Exploding_Fist",
-      "Wizardry",
-      "Wizball",
-      "Yie_Ar_Kung_Fu",
-      "Zamzara",
-      "Zig_Zag",
-      "Zoids"
-    ]
-  }
+  constructor() {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.setButton(this.playVideo);
     this.video = document.getElementById('bgvid') as HTMLVideoElement;
     this.screen = document.getElementById('screen');
+    this.sids = await this.fetchJSON('assets/json/sids.json');
+  }
+  
+  async fetchJSON(path: string): Promise<string[]> {
+    const response = await fetch(path);
+    return response.json();
   }
 
   setup(): void {
@@ -160,9 +63,13 @@ export class PlayerComponent implements OnInit {
     }
     else {
       this.sidPlayer.playcont();
-      this.playing = true;
-      this.playButton = this.pauseIcon;
+      this.setPlaying();
     }
+  }
+  
+  setPlaying(): void {
+    this.playing = true;
+    this.playButton = this.pauseIcon;
   }
 
   next(): void {
@@ -174,6 +81,7 @@ export class PlayerComponent implements OnInit {
       ++this.subTune;
     }
     this.sidPlayer.start(this.subTune);
+    this.setPlaying();
   }
 
   prev(): void {
@@ -185,6 +93,7 @@ export class PlayerComponent implements OnInit {
       --this.subTune;
     }
     this.sidPlayer.start(this.subTune);
+    this.setPlaying();
   }
 
   toggle(): void {
@@ -204,8 +113,8 @@ export class PlayerComponent implements OnInit {
     }
   }
 
-  setButton = (movie: string): void => {
-    this.videoButton = movie;
+  setButton = (video: string): void => {
+    this.videoButton = video;
   }
 
   initTune= () => {
@@ -219,18 +128,19 @@ export class PlayerComponent implements OnInit {
   }
 
   showPlaytime= () => {
-    var time = this.sidPlayer.getplaytime();
-    var minutes = Math.floor(time / 60);
-    var seconds = time % 60;
+    const time = this.sidPlayer.getplaytime();
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
     this.playTime = this.prependZero(minutes) + ':' + this.prependZero(seconds);
   }
 
-  prependZero(num: any): string {
-    return num < 10 ? '0' + num : num;
+  prependZero(num: number): string {
+    return num.toString().padStart(2, '0');
   }
   
   selectChange($event) {
     if (this.sidPlayer) {
+      this.subTune = 0;
       this.sidPlayer.loadinit('assets/sids/' + this.selectedSid + '.sid', this.subTune);
     }
   }
