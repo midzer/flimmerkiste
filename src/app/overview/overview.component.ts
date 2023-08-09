acd469e2f9b4871ea67c79cff7e52c626c390b26
midzer <midzer@gmail.com>
2023-08-09 22:29:15 +0200

    introduce clickable categories

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { POSTS } from '../posts'
+import { POSTS } from '../posts';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-overview',
@@ -9,4 +10,17 @@ import { POSTS } from '../posts'
 })
 export class OverviewComponent {
   posts = POSTS;
+
+  toggleCategory(event) {
+    const badge = event.target;
+    if (badge.classList.contains('active')) {
+      this.posts = POSTS;
+    }
+    else {
+      this.posts = this.posts.filter((post: Post) => {
+        return post.category.includes(event.target.textContent);
+      });
+    }
+    badge.classList.toggle('active');
+  }
 }
