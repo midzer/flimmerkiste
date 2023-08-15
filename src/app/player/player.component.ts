045dc6820b0944a938c20fdc35e397611ceec549
midzer <midzer@gmail.com>
2023-08-15 14:35:56 +0200

    externalize tunes

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 declare var ScripTracker: any;
 declare var jsSID: any;
@@ -8,7 +8,7 @@ declare var jsSID: any;
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent implements AfterViewInit {
   modPlayer: any;
   sidPlayer: any;
   subTune: number = 0;
@@ -37,117 +37,10 @@ export class PlayerComponent implements OnInit {
   optgroupLabel: string = 'SIDS';
   loadedTune: string;
   selectedTune: string = 'Last_Ninja_2';
-  mods: string[] = [
-    "beek-substitutionology",
-    "contraduct_design",
-    "db_3dg"
-  ];
-  sids: string[] = [
-    "ACE_II",
-    "Antics_Dulcedo_Cogitationis",
-    "Arkanoid",
-    "Ark_Pandora",
-    "Armalyte",
-    "Auf_Wiedersehen_Monty",
-    "Baby_of_Can_Guru",
-    "Batman_long",
-    "Battle_Valley",
-    "Bionic_Commando",
-    "BMX_Kidz",
-    "Bulldog",
-    "Chordian",
-    "Cobra",
-    "Combat_Crazy",
-    "Comic_Bakery",
-    "Commando",
-    "Compleeto",
-    "Cooperation_Demo",
-    "Crazy_Comets_remix",
-    "Crazy_Comets",
-    "Cybernoid_II",
-    "Cybernoid",
-    "Defender_of_the_Crown",
-    "Delta",
-    "DNA_Warrior",
-    "Driller",
-    "Eliminator",
-    "FAME_1",
-    "Gauntlet_III",
-    "Gerry_the_Germ",
-    "Ghosts_n_Goblins",
-    "Ghouls_n_Ghosts",
-    "Glider_Rider",
-    "Grand_Prix_Circuit",
-    "Great_Giana_Sisters",
-    "Green_Beret",
-    "Hawkeye",
-    "Hysteria",
-    "IK_plus",
-    "International_Karate",
-    "Katakis",
-    "Kentilla",
-    "Kinetix",
-    "Knucklebusters",
-    "Last_Ninja",
-    "Last_Ninja_2",
-    "Last_Ninja_3",
-    "Lightforce",
-    "Master_of_Magic",
-    "Mega_Apocalypse",
-    "Miami_Vice",
-    "Mikie",
-    "Monty_on_the_Run",
-    "Mutants",
-    "Myth",
-    "Nemesis_the_Warlock",
-    "Nightdawn",
-    "Ocean_Loader_3",
-    "One_Man_and_his_Droid",
-    "Parallax",
-    "Platoon",
-    "Rambo_First_Blood_Part_II",
-    "Ramparts",
-    "R_I_S_K",
-    "RoboCop",
-    "RoboCop_3",
-    "Rocky_Star",
-    "R-Type",
-    "Sanxion",
-    "Savage",
-    "Scroll_Machine",
-    "Scumball",
-    "Short_Circuit",
-    "Snare",
-    "Spellbound",
-    "Stormlord",
-    "Supremacy",
-    "Sweet",
-    "Synth_Sample",
-    "SYS4096",
-    "Terra_Cresta",
-    "Tetris",
-    "Thing_Bounces_Back",
-    "Thrust",
-    "Thunderforce",
-    "Times_of_Lore",
-    "To_be_on_Top",
-    "Trap",
-    "Turbo_Outrun",
-    "Uridium",
-    "Vikings",
-    "Warhawk",
-    "Way_of_the_Exploding_Fist",
-    "Wizardry",
-    "Wizball",
-    "Yie_Ar_Kung_Fu",
-    "Zamzara",
-    "Zig_Zag",
-    "Zoids"
-  ];
+  mods: string[] = [];
+  sids: string[] = [this.selectedTune];
 
-  constructor() {}
-
-  ngOnInit() {
+  ngAfterViewInit() {
     this.canvas = document.querySelector('canvas');
     this.ctx = this.canvas.getContext('2d');
     this.backBuffer = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
@@ -185,6 +78,22 @@ export class PlayerComponent implements OnInit {
         }
       }
     }
+
+    Promise.all([
+      this.fetchJSON('mods'),
+      this.fetchJSON('sids')
+    ])
+    .then(([mods, sids]) => {
+      this.mods = mods;
+      this.sids = sids;
+    });
+  }
+
+  trackByFunc = (index: number, value: string) => value;
+
+  async fetchJSON(file): Promise<any> {
+    const response = await window.fetch(`assets/json/${file}.json`);
+    return await response.json();
   }
 
   setup(label: string): void {
